2d152167b08b992b38fca5cedb75dc5c833299c0
Nick <naccuardi@gmail.com>
2016-11-09 20:19:28 -0800

    some getters/setters have stuff

--- app/src/main/java/edu/up/cs301/Criggabage/CribageMain.java
+++ app/src/main/java/edu/up/cs301/Criggabage/CribageMain.java
@@ -11,11 +11,27 @@ import edu.up.cs301.game.config.GameConfig;
 
 public class CribageMain extends GameMainActivity {
 
+    private int score0;//player 0's score
+    private int score1;//player 1's score
+    private int dealer;//id of the player who is the dealer
+    private int whoseTurn;//id of the player who is allowed to play
+    private boolean winner;//becomes true when a player wins
+    private boolean go;//becomes true when a player must go
+    private int playerId;//determines who is playing
+    private Deck mainDeck = new Deck();//holds 52 cards
+    private Deck player0Hand = new Deck();//is player 0's hand
+    private Deck player0ToPlay = new Deck();//tracks the cards that player 0puts into play
+    private Deck player1Hand = new Deck();//is player 1's hand
+    private Deck player1ToPlay = new Deck();//tracks the cards that player 1 puts into play
+    private Deck cribDeck = new Deck();//contains the cards that players have put in crib
+    private Deck playDeck = new Deck();//contains the cards that players have player
+    private Deck cutDeck = new Deck();//contains the card that was cut from deck
+
+
     @Override
-    public GameConfig createDefaultConfig() {
-
-
-return null;
+    public GameConfig createDefaultConfig()
+    {
+        return null;
     }//end createDefualtConfig()
 
 
@@ -24,27 +40,87 @@ return null;
         return null;
     }//End createLocalGame()
 
+    public void CribageMain()
+    {
+        score0 = 0;
+        score1 = 0;
+        dealer = getDealer();
+        whoseTurn = getWhoseTurn();
+        winner = false;
+        go = false;
+        playerId = getPlayerId();
+        mainDeck.add52();
+        player0Hand = null;
+        player1Hand = null;
+        cribDeck = null;
+        playDeck = null;
+        cutDeck = null;
+        player0ToPlay = null;
+        player1Hand = null;
+    }
     //SETTERS AND GETTERS
     //sets a players score
     public void setScore(int player, int score)
     {
-
+        if(player == 0)
+        {
+            score0 = score0 + score;
+        }
+        else if(player == 1)
+        {
+            score1 = score1 + score;
+        }
+        else
+        {
+            return;
+        }
     }
     //returns a players score
     public int getScore(int player)
     {
+        if(player == 0)
+        {
+            return score0;
+        }
+        else if(player == 1)
+        {
+            return score1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    //sets the players id
+    public void setPlayerId()
+    {
+
+    }
+    //gets the player id
+    public int getPlayerId()
+    {
         return 0;
     }
     //sets the hand for a player
-    public void setHand(int player, Deck[] hand)
+    public void setHand(int player)
     {
 
     }
     //gets the hand of a player
-    public Deck[] getHand(int player)
+    public Deck getHand(int player)
     {
-        Deck[] hand = new Deck[5];
-        return hand;
+        if(player == 0)
+        {
+            return player0Hand;
+        }
+        else if(player == 1)
+        {
+            return player1Hand;
+        }
+        else
+        {
+            return null;
+        }
     }
     //set which players turn it is
     public void setWhoseTurn()
@@ -91,7 +167,12 @@ return null;
     //places six cards in each players hands
     public void deal()
     {
-
+        int handSize = 6;
+        for(int i = 0; i<handSize; i++)
+        {
+            mainDeck.moveTopCardTo(player0Hand);
+            mainDeck.moveTopCardTo(player1Hand);
+        }
     }
     //cuts the deck, returns a single card
     public Deck cut()
