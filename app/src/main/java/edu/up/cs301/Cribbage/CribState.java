ef6d76661c57171e8f40a8189d5c3329f621067d
NAccuardi <naccuardi@gmail.com>
2016-11-14 16:08:01 -0800

    CribMain has the default config made.

---
+++ app/src/main/java/edu/up/cs301/Cribbage/CribState.java
@@ -0,0 +1,10 @@
+package edu.up.cs301.Cribbage;
+
+import edu.up.cs301.game.infoMsg.GameState;
+
+/**
+ * Created by Robot Laptop on 11/14/2016.
+ */
+
+public class CribState extends GameState {
+}
